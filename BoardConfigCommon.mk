cf0ce8b2a12d3e6c8739bbe9af801ab619d0fec7
bxySo <bxyso@outlook.com>
2025-04-11 03:46:09 +0800

    Revert "sm8250-common: Migrate to restructured sepolicy_vndr"
    This reverts commit 7963be8e3ac161bdadf097708e10fe8e9d480788.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -194,7 +194,7 @@ endif
 VENDOR_SECURITY_PATCH := 2024-09-01
 
 # Sepolicy
-include device/qcom/sepolicy_vndr/SEPolicy.mk
+include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/public
